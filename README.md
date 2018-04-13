99d8b879ff292739a933a39d3c993c2ed88159f5
Baiutti Joris <Joris.Baiutti@swisscom.com>
2018-04-13 14:08:40 +0200

    some code

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# This Repository is used for the mqtt gateway for raspberry's
+Unix: [![Unix Build Status](https://img.shields.io/travis/jacebrowning/template-python-demo/master.svg)](https://travis-ci.org/jacebrowning/template-python-demo) Windows: [![Windows Build Status](https://img.shields.io/appveyor/ci/jacebrowning/template-python-demo/master.svg)](https://ci.appveyor.com/project/jacebrowning/template-python-demo)<br>Metrics: [![Coverage Status](https://img.shields.io/coveralls/jacebrowning/template-python-demo/master.svg)](https://coveralls.io/r/jacebrowning/template-python-demo) [![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/jacebrowning/template-python-demo.svg)](https://scrutinizer-ci.com/g/jacebrowning/template-python-demo/?branch=master)<br>Usage: [![PyPI Version](https://img.shields.io/pypi/v/mqttgateway.svg)](https://pypi.org/project/mqttgateway)
+
+# Overview
+
+Sample project generated from jacebrowning/template-python.
+
+This project was generated with [cookiecutter](https://github.com/audreyr/cookiecutter) using [jacebrowning/template-python](https://github.com/jacebrowning/template-python).
+
+# Setup
+
+## Requirements
+
+* Python 3.3+
+
+## Installation
+
+Install mqttgateway with pip:
+
+```sh
+$ pip install mqttgateway
+```
+
+or directly from the source code:
+
+```sh
+$ git clone https://github.com/jacebrowning/template-python-demo.git
+$ cd template-python-demo
+$ python setup.py install
+```
+
+# Usage
+
+After installation, the package can imported:
+
+```sh
+$ python
+>>> import demo
+>>> demo.__version__
+```
---
+++ gateway.py
@@ -0,0 +1,57 @@
+import time
+import socket
+import threading
+import paho.mqtt.client as mqtt
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_temperature import BrickletTemperature
+from tinkerforge.bricklet_gps_v2 import BrickletGPSV2
+from tinkerforge.bricklet_barometer import BrickletBarometer
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    
+
+def sendMessage(client, pressure,temprature,longitude,latitude, altitude):
+    # timestamp; pressure; temprature; longitude, latitude, altitude
+    
+    payload = str(time.time()) + ";" + str(pressure) + ";" + str(temprature) + ";" + str(longitude) + ";" + str(latitude) + ";" + str(altitude)
+    topic = "collectors/" + str(socket.gethostname()) + "/metrics"
+    client.publish(topic, payload, qos=0, retain=False)
+    print(socket.gethostname())
+
+
+
+HOST = "localhost"
+PORT = 4223
+TEMPUID = "dR1" # Change XYZ to the UID of your Temperature Bricklet
+BARUID = "k5Q"
+GPSUID = "qGr"
+
+
+if __name__ == "__main__":
+    ipcon = IPConnection() 
+    tempBricklet = BrickletTemperature(TEMPUID, ipcon)
+    barometerBricklet = BrickletBarometer(BARUID, ipcon)
+    gpsBricklet = BrickletGPSV2(GPSUID, ipcon) 
+    ipcon.connect(HOST, PORT) 
+    client = mqtt.Client(client_id=str(socket.gethostname()))
+    client.on_connect = on_connect
+    client.tls_set("cert.pem")
+    client.username_pw_set("rpi", "p00rT7daH7Lnb0HzMfA0d+zY2fAOo3")
+    client.connect("dapo.0x80.ch", 8883, 30)
+
+    while(True):
+        time.sleep(5)
+        latitude, ns, longitude, ew = gpsBricklet.get_coordinates()
+        altitude = gpsBricklet.get_altitude()
+        temperature = tempBricklet.get_temperature()
+        air_pressure = barometerBricklet.get_air_pressure()
+        sendMessage(client, air_pressure,temperature,longitude,latitude,altitude)
+
+
+    
+
+
+
+
